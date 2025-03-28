0868e7813cc86173bdbfebbb1daa0ef4f575c2e2
Cegatoflo <cesargabrieltorresflorez@gmail.com>
2025-03-28 12:03:59 -0500

    MetodosDivision
    partes de los metodos

--- Factorial/src/factorial/Factorial.java
+++ Factorial/src/factorial/Factorial.java
@@ -18,5 +18,32 @@ public class Factorial {
         int a =20;
         int b = 30;
     }
-    
+
+    // n es la variable global
+
+    //recursivo para calcular el factorial
+    public static long factorialRecursivo(int n) {
+        n = 0;
+    }
+
+    // si un numero es mayor a 0 y no negativos
+    public static boolean esMayorAZero(int n) {
+
+    }
+
+    //verificar si un número es entero (no fracciones ni decimales)
+    public static boolean esEntero(double num) {
+
+    }
+
+    // calcular factorial de números grandes utilizando BigInteger (usa esto)
+    public static java.math.BigInteger factorialGrande(int n) {
+
+    }
+
+    // verificar si es válido (no letras ni caracteres)
+    public static boolean esNumeroValido(String input) {
+
+    }
+
 }
